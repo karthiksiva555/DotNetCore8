6ab579f8fc6fb3de54e5a562968ffee74f6c01fd
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2024-08-11 14:54:18 -0400

    Layout views and partial views practice

---
+++ RazorViews/Controllers/PartialDemoController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RazorViews.Controllers
+{
+    [Route("[controller]")]
+    public class PartialDemoController : Controller
+    {
+        private readonly ILogger<PartialDemoController> _logger;
+
+        public PartialDemoController(ILogger<PartialDemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        
+    }
+}
