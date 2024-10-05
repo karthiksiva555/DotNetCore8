36895e1822f4429d164ef2f643d5dec429326ac0
Siva Narisetty <sivanarisetty@geotab.com>
2024-10-04 22:04:56 -0400

    Fetch config by keys and hierarchical config

--- ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationExample/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace ConfigurationExample.Controllers
 {
@@ -18,11 +12,23 @@ namespace ConfigurationExample.Controllers
             _configuration = configuration;
         }
 
+        [HttpGet("")]
         public IActionResult Index()
         {
+            ViewBag.MyKey1 = _configuration["MyKey1"]!;
+            ViewBag.MyKey2 = _configuration.GetValue<string>("MyKey2", "Default Key2")!;
+            
+            // ViewBag.ClientId = _configuration["WeatherApi:ClientId"]!;
+            // ViewBag.ClientSecret = _configuration.GetValue<string>("WeatherApi:ClientSecret", "Default Client Secret")!;
+            
+            var section = _configuration.GetSection("WeatherApi");
+            ViewBag.ClientId = section["ClientId"]!;
+            ViewBag.ClientSecret = section.GetValue<string>("ClientSecret", "Default Client Secret")!;
+            
             return View();
         }
 
+        [HttpGet("Error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
